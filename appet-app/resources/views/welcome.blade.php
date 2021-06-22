d7b7c264334f9fc82acaa0e89020eb9a259a811d
Luís Fernando Benatto <luisbenatto5@gmail.com>
2021-06-21 21:34:36 -0300

    Main page modifications

--- appet-app/resources/views/welcome.blade.php
+++ appet-app/resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                 <div style="text-align:center; font-size:2rem;">
                     <p>Hello, World!!!</p>
                 </div>
-
+                <!--
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
@@ -103,10 +103,10 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>-->
 
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <div class="text-center text-sm text-gray-500 sm:text-left">
+                <div class="flex justify-center mt-4 sm:items-center">
+                    <!-- <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
                             <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
                                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
@@ -124,9 +124,9 @@
                                 Sponsor
                             </a>
                         </div>
-                    </div>
+                    </div> -->
 
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    <div class="text-center text-sm text-gray-500">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
