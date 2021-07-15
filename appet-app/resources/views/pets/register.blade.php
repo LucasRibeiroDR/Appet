57073c02c7b5ad22ded048778280eca728122185
Luís Fernando Benatto <luisbenatto5@gmail.com>
2021-07-14 22:27:28 -0300

    Correção dos conflitos

--- appet-app/resources/views/pets/register.blade.php
+++ appet-app/resources/views/pets/register.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Criar animal</title>
+    <link rel="shortcut icon" href="pet.ico" type="image/x-icon">
+    <title>Cats&Dogs | Criar animal</title>
 </head>
 <body>
     <h2>Crie seu animal</h2>
--- appet-app/resources/views/welcome.blade.php
+++ appet-app/resources/views/welcome.blade.php
@@ -4,13 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="pet.ico" type="image/x-icon">
-        <title>Cats&Dogs  | app</title>
+        <title>Cats&Dogs | app</title>
     </head>
     <body >
         <div>
-            <div style="text-align:center; font-size:2rem;">
+            <div style="text-align:center; font-size:1rem;">
                 <p>Hello, World!!!</p>
                 <a href="/user/register">User register</a>
+                <a href="/pets/register">Pets register</a>
             </div>
             <div style="text-align:center; font-size:2rem;">
                 Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
