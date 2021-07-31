5d56aeb4867fe1512603f1f2bb5a607e0072a718
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2021-07-31 20:10:16 -0300

    add seeds

---
+++ appet-app/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Bob Esponja',
+            'email' => 'bob@teste.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
