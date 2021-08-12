f849f5e330317be01e093188d4afdfbbe999df69
Antonio Hajime <orozimbo.ao@gmail.com>
2021-08-12 20:42:26 -0300

    Update create.blade.php

--- appet-app/resources/views/pets/create.blade.php
+++ appet-app/resources/views/pets/create.blade.php
@@ -1,41 +1,40 @@
 @extends('layouts.main')
 
-@section('title', 'APPet | Cadastrar pet')
+@section('title', 'APPet | Novo Pet')
 @section('content')
 <div id="event-create-container" class="col-md-6 offset-md-3">
     <h1>Cadastre seu Pet</h1>
     <form action="/pets" method="POST" enctype="multipart/form-data">
     @csrf
         <div class="form-group">
-            <label for="name">Nome</label>
+            <label for="name">Nome do Pet</label>
             <input type="text" class="form-control" id="name" name="name" placeholder="Bob, Nina, Belinha, ...">
         </div>
         <div class="form-group">
-            <label for="raca">Raça</label>
+            <label for="raca">Raça do Pet</label>
             <input type="text" class="form-control" id="raca" name="raca" placeholder="Pinscher, poodle, pug, chihuahua, ...">
         </div>
         <div class="form-group">
-            <label for="raca">Raça do pet</label>
-            <input type="text" class="form-control" id="raca" name="raca" placeholder="Pinscher, poodle, pug, chihuahua,...">
+            <label for="pelugem">Cor da pelagem</label>
+            <input type="text" class="form-control" id="pelugem" name="pelugem" placeholder="Marrom claro, marrom escuro, preto, ...">
         </div>
         <div class="form-group">
-            <label for="especie">Especie</label>
-            <input type="text" class="form-control" id="especie" name="especie" placeholder="Caninos, felinos, ...">
+            <label for="especie">Especie do seu Pet</label>
+            <input type="text" class="form-control" id="especie" name="especie" placeholder="Cachorro, gato, ...">
         </div>
-
         <div class="form-group">
             <label for="data_nascimento">Data de nascimento</label>
             <input type="date" class="form-control" name="data_nascimento" id="data_nascimento">
         </div>
         <div class="form-group">
             <label for="castrado">O animal é castrado?</label>
-            <select name="castrado" id="castrado" class="form-control" placeholder="Selecione">
+            <select name="castrado" id="castrado" class="form-control">
               <option value="0">Não</option>
               <option value="1">Sim</option>
             </select>
         </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Cadastrar Animal">
+            <input type="submit" class="btn btn-primary" value="Adicionar Animal">
         </div>
     </form>
 </div>
