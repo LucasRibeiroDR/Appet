9ad435784cfdfd37130dd5a706f1eb830aef0a2b
Beatriz Rodrigues <beatriz.ap.freitas.rodrigues@gmail.com>
2021-08-26 23:25:29 -0300

    Ajustando tela de login e cadastro MBF

--- appet-app/resources/views/auth/register.blade.php
+++ appet-app/resources/views/auth/register.blade.php
@@ -3,52 +3,43 @@
 
 <x-guest-layout>
 
-<link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/user/forms.css">
 
-    <div class="justify">
-        <div class="containerCad">
-
-            <div class="cadastro-content">
-                <form method="POST" action="{{ route('register') }}">
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <form class="login100-form validate-form" method="POST" action="{{ route('register') }}">
                     @csrf
-                    <h2 class="titleText">Cadastrar</h2>
-                    <div>
-                        <x-jet-label class="alignText" for="name" value="{{ __('Nome completo:') }}" />
-                        <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Seu nome..." style="border-color: #1D4B80;" />
+                    <span class="login100-form-title">
+                        Cadastrar
+                    </span>
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="name" class="input100" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="{{ __('Nome completo') }}"/>
                     </div>
-                    <div>
-                        <div class="blc">
-                            <x-jet-label class="alignText" for="cpf" value="{{ __('CPF:') }}" />
-                            <x-jet-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" required autocomplete="new-cpf" placeholder="000.000.000-00" style="border-color: #1D4B80;" />
-                        </div>
-
-                        <div class="blc">
-                            <x-jet-label class="alignText" for="rg" value="{{ __('RG:') }}" />
-                            <x-jet-input id="rg" class="block mt-1 w-full" type="text" name="rg" required autocomplete="new-rg" placeholder="00.000.000-0" style="border-color: #1D4B80;" />
-                        </div>
-                    </div>
-                    <div>
-                        <x-jet-label class="alignText" for="telefone" value="{{ __('Telefone:') }}" />
-                        <x-jet-input id="telefone" class="block mt-1 w-full" type="text" name="telefone" required autocomplete="new-telefone" placeholder="(00) 00000-0000" style="border-color: #1D4B80;" />
-                    </div>
-                    <div>
-                        <x-jet-label class="alignText" for="endereco" value="{{ __('Endereço:') }}" />
-                        <x-jet-input id="endereco" class="block mt-1 w-full" type="text" name="endereco" required autocomplete="new-endereco" placeholder="Coloque seu endereço..." style="border-color: #1D4B80;" />
-                    </div>
-                    <div>
-                        <x-jet-label class="alignText" for="email" value="{{ __('E-mail:') }}" />
-                        <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required placeholder="Coloque seu email..." style="border-color: #1D4B80;" />
-                    </div>
-                    <div class="blc">
-                        <x-jet-label class="alignText" for="password" value="{{ __('Senha:') }}" />
-                        <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" placeholder="Informe sua senha..." style="border-color: #1D4B80;" />
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="cpf" class="input100" type="text" name="cpf" required autocomplete="new-cpf" placeholder="{{ __('CPF') }}"/>
                     </div>
 
-                    <div class="blc">
-                        <x-jet-label class="alignText" for="password_confirmation" value="{{ __('Confirmar Senha:') }}" />
-                        <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Confirme sua senha..." style="border-color: #1D4B80;" />
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="rg" class="input100" type="text" name="rg" required autocomplete="new-rg" placeholder="{{ __('RG') }}"/>
                     </div>
-                    <br><br><br>
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="telefone" class="input100" type="text" name="telefone" required autocomplete="new-telefone" placeholder="{{ __('Telefone') }}"/>
+                    </div>
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="endereco" class="input100" type="text" name="endereco" required autocomplete="new-endereco" placeholder="{{ __('Endereço') }}"/>
+                    </div>
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="email" class="input100" type="email" name="email" :value="old('email')" required placeholder="{{ __('E-mail') }}"/>
+                    </div>
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="password" class="input100" type="password" name="password" required autocomplete="new-password" placeholder="{{ __('Senha') }}"/>
+                    </div>
+
+                    <div class="wrap-input100 validate-input">
+                        <x-jet-input id="password_confirmation" class="input100" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="{{ __('Confirmar Senha') }}"/>
+                    </div>
+
                     @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                     <div class="mt-4">
                         <x-jet-label for="terms">
@@ -66,19 +57,21 @@
                     </div>
                     @endif
 
-                    <div class="flex items-center justify-end mt-4">
-                        <a href="{{ route('login') }}">
-                            {{ __('Já está registrado?') }}
-                        </a>
-
-                        <x-jet-button class="btn btn-primary ml-4">
-                            {{ __('Cadastrar-se') }}
+                    <div class="container-login100-form-btn">
+                        <x-jet-button class="login100-form-btn">
+                        {{ __('Cadastrar-se') }}
                         </x-jet-button>
                     </div>
+
+                    <div class="text-center p-t-20">
+                        <a class="txt" href="{{ route('login') }}">
+                            {{ __('Já está registrado?') }}
+                        </a>
+                    </div>
                 </form>
-            </div>
-            <div class="imgLogin">
-                <img src="./img/2.png" alt="calendario">
+                <div class="login100-pic">
+                    <img src="{{ asset('./img/2.png') }}" alt="self cachorro">
+                </div>
             </div>
         </div>
     </div>
--- appet-app/resources/views/welcome.blade.php
+++ appet-app/resources/views/welcome.blade.php
@@ -1,21 +1,29 @@
 @extends('layouts.main')
-
+@extends('layouts.app')
 @section('title', 'APPet')
 @section('content')
 
-<div>
-  <div class="col-md-6 offset-md-1 " style="margin-top: 100px;">
-    <h1>Agende a consulta do seu animalzinho pelo site do HV UENP!</h1>
-    <ul class="lista mt-3">
-      <li>Agende consulta sem sair de casa</li>
-      <li>Salve os dados do seu animal no nosso sistema</li>
-      <li>Receba notificações no dia da consulta</li>
-    </ul>
-    <button class="w-100 btn btn-outline-primary mt-3">Agendar consulta</button>
+<body class="header">
+  <div class="container">
+    <div class="row d-flex ">
+      <div class="col-lg-6 col-xl-6">
+        <div >
+          <h1 class="h1-large">Agende a consulta do seu animalzinho pelo site do HV UENP!</h1>
+          <ul>
+            <li>Agende consulta sem sair de casa</li>
+            <li>Salve os dados do seu animal no nosso sistema</li>
+            <li>Receba notificações no dia da consulta</li>
+          </ul>
+          <button class="w-100 btn btn-outline-primary mt-3">Agendar consulta</button>
+        </div>
+      </div>
+      <div class="col-lg-6 col-xl-6">
+        <div class="image-container">
+          <img class="img-fluid" src="./img/tel.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="col-md-6 offset-md-1 dashboard-pets-container">
-    <img class="imagemHome" src="./img/tel.png" alt="">
-  </div>
-  <!-- <a href="/user/create">User create</a> -->
-</div>
+</body>
+
 @endsection
