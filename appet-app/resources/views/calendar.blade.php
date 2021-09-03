574553a1e6e42ec46e7e3755c0014a862fee2ceb
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2021-09-03 18:21:18 -0300

    Visual Calendar

--- appet-app/resources/views/calendar.blade.php
+++ appet-app/resources/views/calendar.blade.php
@@ -106,20 +106,15 @@
         return $totalAppointments;
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/globalColors.css">
-    <link rel="stylesheet" href="/css/styles.css">
-    <link rel="stylesheet" href="/css/calendar.css">
-    <title>Calendar - PetsOn</title>
-    <style> </style>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('title', 'PetsOn | Calendário')
+@section('content')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/globalColors.css">
+<link rel="stylesheet" href="/css/styles.css">
+<link rel="stylesheet" href="/css/calendar.css">
+
 <div class="container"> 
   <div class="row"> 
    <div class="col-md-12"> 
@@ -139,5 +134,4 @@
    </div> 
   </div> 
  </div>
-</body>
-</html>
+@endsection
--- appet-app/resources/views/welcome.blade.php
+++ appet-app/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'APPet')
+@section('title', 'PetsOn')
 @section('content')
 
 <div>
